d982001680532be088f73be65c3ce976477689ba
DeeDee43 <idellewd@gmail.com>
2017-06-09 14:58:01 -0500

    Added Plugin Update Checker

--- social-warfare-social-proof-widget.php
+++ social-warfare-social-proof-widget.php
@@ -18,3 +18,17 @@ define( 'SW_SPW_VERSION' , '1.0.0' );
 define( 'SW_SPW_PLUGIN_FILE', __FILE__ );
 define( 'SW_SPW_PLUGIN_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'SW_SPW_PLUGIN_DIR', dirname( __FILE__ ) );
+
+/**
+ * The Plugin Update checker
+ *
+ * @since 2.0.0
+ * @access public
+ */
+require_once SW_SPW_PLUGIN_DIR . '/update-checker/plugin-update-checker.php';
+$swps_github_checker = swp_PucFactory::getLatestClassVersion('PucGitHubChecker');
+$sw_SPW_update_checker = new $swpp_github_checker(
+    'https://github.com/warfare-plugins/social-warfare-shortcodes/',
+    __FILE__,
+    'master'
+);
