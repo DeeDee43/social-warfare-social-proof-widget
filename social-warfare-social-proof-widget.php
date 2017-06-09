86280f944900df9e92358585cfead5ae01109246
DeeDee43 <idellewd@gmail.com>
2017-06-09 14:53:49 -0500

    Created SW-SPW docblock

--- social-warfare-social-proof-widget.php
+++ social-warfare-social-proof-widget.php
@@ -1,1 +1,10 @@
 <?php
+/**
+ * Plugin Name: Social Warfare - Social Proof Widget
+ * Plugin URI:  http://warfareplugins.com
+ * Description: A plugin that adds fun and useful shortcodes for the Social Warfare plugin.
+ * Version:     1.0.0
+ * Author:      Warfare Plugins
+ * Author URI:  http://warfareplugins.com
+ * Text Domain: social-warfare
+*/
