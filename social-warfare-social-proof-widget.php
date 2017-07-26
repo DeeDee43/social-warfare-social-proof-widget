c6d0e08ebe0cb0f6227fa1b3443c816f86aa01bb
DeeDee43 <idellewd@gmail.com>
2017-07-26 15:10:30 -0500

    Trouble with $instance.

--- social-warfare-social-proof-widget.php
+++ social-warfare-social-proof-widget.php
@@ -51,12 +51,23 @@ class Social_warfare_social_proof_widget extends WP_Widget {
 		$googlePlus = $instance['googlePlus'];
 		$twitter = $instance['twitter'];
 		$facebook = $instance['facebook'];
+		$linkedIn = $instance['linkedIn'];
+		$pinterest= $instance['pinterest'];
+		$stumbleupon = $instance['stumbleupon'];
+		$tumblr = $instance['tumblr'];
+		$reddit = $instance['reddit'];
+		$yummly = $instance['yummly'];
+		$buffer = $instance['buffer'];
+		$hacker_news = $instance['hacker_news'];
+		$flipboard = $instance['flipboard'];
+
+
 
 		echo $before_widget;
 		echo $before_title . $title . $after_title;
 		echo $after_widget;
 
-		var_dump($instance);
+		//var_dump($instance);
 
 	}
 
@@ -69,14 +80,32 @@ class Social_warfare_social_proof_widget extends WP_Widget {
 		$instance['facebook'] = strip_tags($new_instance['facebook']);
 		$instance['linkedIn'] = strip_tags($new_instance['linkedIn']);
 		$instance['pinterest'] = strip_tags($new_instance['pinterest']);
+		$instance['stumbleupon'] = strip_tags($new_instance['stumbleupon']);
+		$instance['tumblr'] = strip_tags($new_instance['tumblr']);
+		$instance['reddit'] = strip_tags($new_instance['reddit']);
+		$instance['yummly'] = strip_tags($new_instance['yummly']);
+		$instance['buffer'] = strip_tags($new_instance['buffer']);
+		$instance['hacker_news'] = strip_tags($new_instance['hacker_news']);
+		$instance['flipboard'] = strip_tags($new_instance['flipboard']);
         return $instance;
 	}
 
 	function form( $instance ) {
 		// Output admin widget options form
 
-		$title = esc_attr($instance['title']);
-		$googlePlus = $instance['googlePlus'];
+		$title = esc_attr( $instance['title'] );
+		$googlePlus = esc_attr($instance['googlePlus']);
+		$twitter = esc_attr( $instance['twitter'] );
+		$facebook = esc_attr($instance['facebook']);
+		$linkedIn = esc_attr($instance['linkedIn']);
+		$pinterest= esc_attr($instance['pinterest']);
+		$stumbleupon = $instance['stumbleupon'];
+		$tumblr = $instance['tumblr'];
+		$reddit = $instance['reddit'];
+		$yummly = $instance['yummly'];
+		$buffer = $instance['buffer'];
+		$hacker_news = $instance['hacker_news'];
+		$flipboard = $instance['flipboard'];
 
 		$icons_array = apply_filters( 'swp_button_options' , $icons_array );
 
@@ -92,13 +121,74 @@ class Social_warfare_social_proof_widget extends WP_Widget {
 			<label> Pick a Network: </label></br>
 			<?php
 			foreach( $icons_array['content'] as $button => $network ) :
-				//var_dump($value);
 				?>
-				<input class="widefat" id="<?php echo $button;?>" name="<?php echo esc_attr($network['content']) ?>" type="checkbox" value= '0'<?php checked($button,1);  ?>>
+				<input class="widefat"
+				<?php
+				if($button == 'googlePlus'){
+					?>
+					id="<?php echo $this->get_field_id('googlePlus');?>" name="<?php echo $this->get_field_name('googlePlus'); ?>" type="checkbox"
+					value= "1"  <?php checked($button,1 ); ?>
+					<?php
+				}elseif ($button == 'twitter'){
+					?>
+					id="<?php echo $this->get_field_id('twitter');?>" name="<?php echo $this->get_field_name('twitter'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}elseif ($button == 'facebook'){
+					?>
+					id="<?php echo $this->get_field_id('facebook');?>" name="<?php echo $this->get_field_name('facebook'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}elseif ($button == 'pinterest'){
+					?>
+					id="<?php echo $this->get_field_id('pinterest');?>" name="<?php echo $this->get_field_name('pinterest'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}elseif ($button == 'linkedIn'){
+					?>
+					id="<?php echo $this->get_field_id('linkedIn');?>" name="<?php echo $this->get_field_name('linkedIn'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}elseif ($button == 'stumbleupon'){
+					?>
+					id="<?php echo $this->get_field_id('stumbleupon');?>" name="<?php echo $this->get_field_name('stumbleupon'); ?>" type="checkbox"
+					value= "1" <?php checked($button,'1'); ?>
+					<?php
+				}elseif ($button == 'tumblr'){
+					?>
+					id="<?php echo $this->get_field_id('tumblr');?>" name="<?php echo $this->get_field_name('tumblr'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}elseif ($button == 'reddit'){
+					?>
+					id="<?php echo $this->get_field_id('reddit');?>" name="<?php echo $this->get_field_name('reddit'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}elseif ($button == 'yummly'){
+					?>
+					id="<?php echo $this->get_field_id('yummly');?>" name="<?php echo $this->get_field_name('yummly'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}elseif ($button == 'buffer'){
+					?>
+					id="<?php echo $this->get_field_id('buffer');?>" name="<?php echo $this->get_field_name('buffer'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}elseif ($button == 'hacker_news'){
+					?>
+					id="<?php echo $this->get_field_id('hacker_news');?>" name="<?php echo $this->get_field_name('hacker_news'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}elseif ($button == 'flipboard'){
+					?>
+					id="<?php echo $this->get_field_id('flipboard');?>" name="<?php echo $this->get_field_name('flipboard'); ?>" type="checkbox"
+					value= "1" <?php checked($button,1); ?>
+					<?php
+				}
+
 				<?php
 				echo $network['content'];
 				echo "<br />";
-
 			endforeach;
 			?>
 		</p>
